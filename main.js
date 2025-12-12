d2ed4d9c5de99d389eed438030f5c35bdff636de
hhseok2002 <hyeonseok2002@naver.com>
2025-12-13 01:40:30 +0900

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-    // 사이드바를 오른쪽에서 화면 안으로 이동 (right: -250px -> right: 0)
-    document.getElementById("mobile-sidebar").style.right = "0";
-}
-
-function closeNav() {
-    // 사이드바를 오른쪽으로 화면 밖으로 이동 (right: 0 -> right: -250px)
-    document.getElementById("mobile-sidebar").style.right = "-250px";
-}
