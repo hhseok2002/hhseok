2e55bd9480408db9c02ed7bcc988f3b4cbd5e3df
hhseok2002 <hyeonseok2002@naver.com>
2025-12-13 01:19:25 +0900

    Delete js directory

--- js/timeless.js
+++
@@ -1,6 +0,0 @@
-            function openNav() {
-                document.getElementById("slide-sidebar").style.left = "0";
-            }
-            function closeNav() {
-                document.getElementById("slide-sidebar").style.left = "-250px";
-            }
